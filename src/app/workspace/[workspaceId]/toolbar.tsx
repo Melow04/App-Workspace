8aacefa20c38ec0e6c13abbf8ce330332c0b96f1
Melow04 <corteskal04@gmail.com>
2024-11-03 19:57:14 +0800

    feat(nav): created nav/toolbar

--- src/app/workspace/[workspaceId]/toolbar.tsx
+++ src/app/workspace/[workspaceId]/toolbar.tsx
@@ -1,10 +1,31 @@
-
-
+import { Button } from "@/components/ui/button";
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { Info, Search } from "lucide-react";
 
 const Toolbar = () => {
+  const workspaceId = useWorkspaceId();
+  const { data } = useGetWorkspace({ id: workspaceId });
+
   return (
-    <div className="bg-red-500">toolbar</div>
+    <nav className="bg-[#7653a1] flex items-center justify-between h-10 p-1.5">
+      <div className="flex-1"/>
+        <div className="min-w-[280px] max-[642px] grow-[2] shrink">
+          <Button
+            size="sm"
+            className=" bg-accent/25 w-full hover:bg-accent/25 justify-start h-7 px-2"
+          >
+            <Search className="size-4 text-white mr-2" />
+            <span className="text-white text-xs">Search {data?.name}</span>
+          </Button>
+        </div>
+        <div className=" ml-auto flex-1 flex items-center justify-end">
+          <Button variant="transparent" size="iconSm" className="items-center justify-center">
+            <Info className=" size-5 text-white" />
+          </Button>
+        </div>
+    </nav>
   );
 };
 
-export default Toolbar
+export default Toolbar;
