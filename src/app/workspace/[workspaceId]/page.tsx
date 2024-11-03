571e075a6d498f4aa35b091c5fe1e222975011a5
Melow04 <corteskal04@gmail.com>
2024-11-03 12:13:36 +0800

    feat(workspace): created id as parameter

---
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -0,0 +1,18 @@
+
+interface WorkSpaceIdPageProps{
+    params:{
+        workspaceId: string;
+    };
+};
+
+
+const WorkSpaceIdPage = ({ params }: WorkSpaceIdPageProps) => {
+  return (
+    <div>
+        ID:{params.workspaceId};
+    </div>
+
+);
+}
+
+export default WorkSpaceIdPage;
