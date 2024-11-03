5941fefe3dae5acbadfc7105ca16236e6b990605
Melow04 <corteskal04@gmail.com>
2024-11-03 15:06:13 +0800

    feat(getter): created a getbyid query

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -2,27 +2,28 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { getAuthUserId } from "@convex-dev/auth/server";
 
-export const create = mutation ({
-    args:{
+
+export const create = mutation({
+    args: {
         name: v.string(),
     },
-    handler:async (ctx, args)=> {
-const userId = await getAuthUserId(ctx);
+    handler: async (ctx, args) => {
+        const userId = await getAuthUserId(ctx);
 
-if(!userId){
-    throw new Error ("Unauthorize");
-}
+        if (!userId) {
+            throw new Error("Unauthorize");
+        }
 
-const joinCode = "123456";
+        const joinCode = "123456";
 
-const workspaceId = await ctx.db.insert("workspaces",{
-    name: args.name,
-    userId, 
-    joinCode,
-});
+        const workspaceId = await ctx.db.insert("workspaces", {
+            name: args.name,
+            userId,
+            joinCode,
+        });
 
 
-return workspaceId;
+        return workspaceId;
     },
 });
 
@@ -30,6 +31,20 @@ return workspaceId;
 export const get = query({
     args: {},
     handler: async (ctx) => {
-    return await ctx.db.query("workspaces").collect();
-},
-})
+        return await ctx.db.query("workspaces").collect();
+    },
+});
+
+export const getById = query({
+    args: { id: v.id("workspaces") },
+    handler: async (ctx, args) => {
+        const userId = await getAuthUserId(ctx);
+
+        if (!userId) {
+            throw new Error("Unauthorized");
+        }
+
+
+        return await ctx.db.get(args.id);
+    },
+});
