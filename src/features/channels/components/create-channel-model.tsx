b129264d0e5d020a58daed0df150fac20b6988cc
Melow04 <corteskal04@gmail.com>
2024-11-05 23:31:27 +0800

    feat(channel): created modal and request type for creating channel

--- src/features/channels/components/create-channel-model.tsx
+++ src/features/channels/components/create-channel-model.tsx
@@ -1,27 +1,72 @@
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-  } from "@/components/ui/dialog"
-  import { useCreateChannelModal } from "../store/use-create-channel-modal";
-  
-  export const  CreateChannelModal = () => {
-    const [open, setOpen] = useCreateChannelModal();
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useCreateChannelModal } from "../store/use-create-channel-modal";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { useCreateChannel } from "../api/use-create-channel";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
 
-    return(
-        <Dialog open={open} onOpenChange={setOpen}>
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>
-                        Add a channel
-                    </DialogTitle>
-                </DialogHeader>
-            </DialogContent>
-        </Dialog>
+export const CreateChannelModal = () => {
+  const workspaceId = useWorkspaceId  ();
+  const [open, setOpen] = useCreateChannelModal();
 
-        
-    );
+  const {mutate, isPending} = useCreateChannel();
+
+  const [name, setName] = useState("");
+
+  const handleClose = () => {
+    setName("");
+    setOpen(false);
   };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.replace(/\s+/g, "-").toLowerCase();
+    setName(value);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    mutate({
+      name, workspaceId
+    },
+    {
+      onSuccess: (id) => {
+        // todo: redirect to
+        handleClose();
+      },
+    },
+  )
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={handleClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add a channel</DialogTitle>
+        </DialogHeader>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <Input
+            value={name}
+            disabled={isPending}
+            onChange={handleChange}
+            placeholder="e.g plan-budget"
+            required
+            autoFocus
+            minLength={3}
+            maxLength={80}
+          />
+          <div className="flex justify-end ">
+            <Button disabled={isPending}>Create</Button>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
